a5b336d534b84c95a4253c107601369b229fc20e
Ammar Magdy <ammar.m.badawi@gmail.com>
2021-12-25 17:12:14 +0200

    add utilis Folder

--- YackeenTask/Modules/Shows/ViewController/Cell/ShowsCollectionCell.swift
+++ YackeenTask/Modules/Shows/ViewController/Cell/ShowsCollectionCell.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ShowsCollectionCell: UICollectionViewCell {
 
+    @IBOutlet weak var showImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var linkLabel: UILabel!
+    @IBOutlet weak var premieredStringLabel: UILabel!
+    @IBOutlet weak var premieredLabel: UILabel!
+    @IBOutlet weak var runtimeStringLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var rateStringLabel: UILabel!
+    @IBOutlet weak var rateView: FloatRatingView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
