7ee8def2d17133a14072a8ed6c58fa44575e73f1
Ammar Magdy <ammar.m.badawi@gmail.com>
2021-12-25 14:21:43 +0200

    setup for run

---
+++ YackeenTask/Modules/LaunchScreen/LaunchScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LaunchScreenViewController.swift
+//  Myto
+//
+//  Created by Afnan MacBook Pro on 30/06/2021.
+//
+
+import UIKit
+
+
+class LaunchScreenViewController: UIViewController {
+        
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
