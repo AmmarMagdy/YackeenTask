b031f0f9fe82dcf6abf5fc30899dad250ca2ed89
Ammar Magdy <ammar.m.badawi@gmail.com>
2021-12-25 17:43:33 +0200

    add view controller routes

--- YackeenTask/Helpers/Extensions/UIViewController+Extension.swift
+++ YackeenTask/Helpers/Extensions/UIViewController+Extension.swift
@@ -5,4 +5,42 @@
 //  Created by Ammar.M on 25/12/2021.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController: NavigationRoute {
+    
+    func popVC() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func navigate(to route: Route) {
+        switch route.style {
+        case .modal(let transtion):
+            self.presentViewController(route.destination, transtion ?? .coverVertical)
+        case .push:
+            pushViewController(route.destination)
+        }
+    }
+    
+    func dismiss(navigateTo route: Route?) {
+        self.dismiss(animated: true)
+        guard let route = route else {return}
+        navigate(to: route)
+    }
+    
+    func presentViewController(_ viewController : UIViewController, _ defaultTranstion: UIModalTransitionStyle = .coverVertical) {
+        viewController.modalPresentationStyle = .overFullScreen
+        viewController.modalTransitionStyle = defaultTranstion
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    func pushViewController(_ viewController : UIViewController) {
+        if type(of: self) == UINavigationController.self {
+            (self as? UINavigationController)?.pushViewController(viewController, animated: true)
+        } else if self.presentingViewController != nil {
+            (self.presentingViewController as? UINavigationController)?.pushViewController(viewController, animated: true)
+        } else {
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+}
